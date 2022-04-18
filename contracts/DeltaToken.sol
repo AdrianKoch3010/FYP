95bee91c2bc55105fedbb2e45e12bc9be81df1d6
AdrianKoch3010 <adriankoch3010@gmail.com>
2022-04-18 11:59:34 +0100

    Initial commit

---
+++ contracts/DeltaToken.sol
@@ -0,0 +1,11 @@
+// contracts/DeltaToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract GLDToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Delta", "DLT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
